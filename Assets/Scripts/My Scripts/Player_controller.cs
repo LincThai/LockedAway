9aca7ff449629ac99caf2814f364231e588537ee
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2021-08-19 11:47:02 +1000

    Modifications to player scripts and testing

--- Assets/Scripts/My Scripts/Player_controller.cs
+++ Assets/Scripts/My Scripts/Player_controller.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Player_controller : MonoBehaviour
 {
     // Set Variablez
-    public float Speed;
+    private float speed;
+    public float walkSpeed;
+    public float runSpeed;
+    public float crouchSpeed;
+
     public float jumpForce;
 
     // reference rigid body
@@ -13,16 +17,25 @@ public class Player_controller : MonoBehaviour
 
 
     public Transform groundCheck;
+
+    CapsuleCollider capCollider;
+
     // a bool for telling if the player is grounded
     public bool isGrounded;
     // if player can jump
     public bool canJump;
+    // if the player can fly
+    public bool canFly;
+    // when player is crouching
+    public bool isCrouch;
 
     // Start is called before the first frame update
     void Start()
     {
-        //referenc rigidbody
+        //reference rigidbody
         myBody = GetComponent<Rigidbody>();
+        // reference to capsule collider
+        capCollider = GetComponent<CapsuleCollider>();
         // stops curser from being seen during gameplay.
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -35,7 +48,7 @@ public class Player_controller : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         // player movement
-        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical) * Speed * Time.deltaTime;
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
         transform.Translate(moveDirection);
 
         // creates a sphere and when that speere hits something it will return isGrounded=true
@@ -45,7 +58,35 @@ public class Player_controller : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             canJump = !canJump;
+        }
 
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            //DoCrouch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && isCrouch)
+        {
+            speed = runSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            speed = walkSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Fly();
+        }
+
+        if (Input.GetKey(KeyCode.Z) && canFly)
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.X) && canFly)
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
         }
     }
 
@@ -58,4 +99,22 @@ public class Player_controller : MonoBehaviour
         }
     }
 
+    public void DoCrouch()
+    {
+        if (isCrouch)
+        {
+            capCollider.height += 1f;
+        }
+        else
+        {
+            capCollider.height - +1f;
+        }
+        isCrouch = !isCrouch;
+    }
+
+    public void Fly()
+    {
+        canFly = !canFly;
+        myBody.isKinematic = canFly;
+    }
 }
