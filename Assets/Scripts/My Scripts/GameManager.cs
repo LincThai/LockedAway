cf4716feb8f99b80e7e4c4d0067f55dd90f94179
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2021-08-26 14:36:53 +1000

    Game manager setup

--- Assets/Scripts/My Scripts/GameManager.cs
+++ Assets/Scripts/My Scripts/GameManager.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // set variables
+    // game state variable setup
+    public GameState gameState;
+
+    // UI variables
+    public GameObject startUI;
+    public GameObject gameUI;
+    public GameObject endUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set gamestate at the start of game
+        gameState = GameState.preGame;
+        // Set up UI's at start of game
+        startUI.SetActive(true);
+        gameUI.SetActive(false);
+        endUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameStateCheck();
+    }
+
+    public void GameStateCheck()
+    {
+        if (gameState == GameState.preGame)
+        {
+            startUI.SetActive(true);
+            gameUI.SetActive(false);
+            endUI.SetActive(false);
+        }
+        else if (gameState == GameState.game)
+        {
+            startUI.SetActive(false);
+            gameUI.SetActive(true);
+            endUI.SetActive(false);
+        }
+        else if (gameState == GameState.end)
+        {
+            startUI.SetActive(false);
+            gameUI.SetActive(false);
+            endUI.SetActive(true);
+        }
     }
 }
+public enum GameState {preGame, game, end};
