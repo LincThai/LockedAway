09ac1b28817c4454bcc78894d16b130f796757ac
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2021-08-26 19:19:09 +1000

    start ui script work

--- Assets/Scripts/My Scripts/StartUI.cs
+++ Assets/Scripts/My Scripts/StartUI.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class StartUI : MonoBehaviour
 {
+    // game manager variable
+    GameManager GM;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // call game manager from the game manager game object
+        GM = GameObject.FindGameObjectOfType<GameManager>().GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        public void OnClickButton(int buttonClicked)
+        {
+            if (buttonClicked == 1)
+            {
+                GM.gameState = GameState.game;
+            }
+            if (buttonClicked == 2)
+            {
+                Application.Quit();
+            }
+        }
     }
 }
